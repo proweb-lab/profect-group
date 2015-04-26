177f70922185aa05aa56ab82cd8b13431c78f745
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-04-26 14:57:59 +0300

    section view

--- sites/all/themes/profect/templates/views/main_works/views-view-fields--section--front-block.tpl.php
+++
@@ -1,9 +0,0 @@
-<a href="#" id="<?php echo $fields['title']->content . '-' . $fields['field_weight']->content; ?>" class="service-box">
- 
-    <div class="plus strategyicon"><?php print $fields['field_logo']->content; ?>
-    </div>
-    <div class="hover-arrow"><img border="0" alt="arrow" src="sites/all/themes/profect/images/arrow.png"></div>
-    <h3><?php  echo $fields['title']->raw; ?></h3>
-    <div class="introduction"><?php print $fields['field_introduction']->content; ?></div>
-
-</a>
