02174fdcdba6778762298e04cf936cae6d66d319
fadi-saadeldin <mf.saadeldin@gmail.com>
2015-04-26 10:15:19 +0300

    new commit

--- sites/all/themes/profect/templates/views/main_works/views-view-fields--section--front-block.tpl.php
+++ sites/all/themes/profect/templates/views/main_works/views-view-fields--section--front-block.tpl.php
@@ -1,8 +1,11 @@
-<div class="service-box service-box<?php print $fields['field_weight']->content;?> ">
+<a href="#" id="<?php echo $fields['title']->raw . '-' . $fields['field_weight']->raw; ?>" class="service-box offices-tab">
+  
+
+
     <div class="plus strategyicon"><?php print $fields['field_logo']->content; ?>
     </div>
     <div class="hover-arrow"><img border="0" alt="arrow" src="sites/all/themes/profect/images/arrow.png"></div>
-    <h3><?php print $fields['title']->content; ?></h3>
+    <h3><?php  echo $fields['title']->raw; ?></h3>
     <div class="introduction"><?php print $fields['field_introduction']->content; ?></div>
-  
-</div>
+
+</a>
